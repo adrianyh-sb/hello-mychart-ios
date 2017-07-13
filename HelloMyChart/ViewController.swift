f1436c263e8ab770ec22d2e2de40ee4a5987a00c
Adrian YH <ayounghoon@adrians-mbp.sw.ca>
2017-07-13 11:51:06 -0400

    Added a print line

--- HelloMyChart/ViewController.swift
+++ HelloMyChart/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Push this project to GitHub")
     }
 
     override func didReceiveMemoryWarning() {
